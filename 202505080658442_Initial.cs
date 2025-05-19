d2a123b69f82827dcc8ac726007b326436554765
Srividhya <assrividhya113@gmail.com>
2025-05-19 10:39:50 +0530

    Add Migration files

---
+++ 202505080658442_Initial.cs
@@ -0,0 +1,41 @@
+﻿namespace CourseEnrollmentSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        CourseId = c.Int(nullable: false, identity: true),
+                        CourseName = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.CourseId);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentId = c.Int(nullable: false, identity: true),
+                        StudentName = c.String(nullable: false),
+                        CourseId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.StudentId)
+                .ForeignKey("dbo.Courses", t => t.CourseId, cascadeDelete: true)
+                .Index(t => t.CourseId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Students", "CourseId", "dbo.Courses");
+            DropIndex("dbo.Students", new[] { "CourseId" });
+            DropTable("dbo.Students");
+            DropTable("dbo.Courses");
+        }
+    }
+}

f8f2d4cfb3a39d49d67ccf04f85df5bff8da09a7
Srividhya <assrividhya113@gmail.com>
2025-05-19 10:20:17 +0530

    Add model  files

---
+++ CourseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CourseEnrollmentSystem.Models
+{
+    public class CourseContext : DbContext
+	{
+        public CourseContext() : base("CourseContext") { }
+        public DbSet<Course> Courses { get; set; }
+		public DbSet<Student> Students { get; set; }
+
+	}
+}
